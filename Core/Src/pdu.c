975e8cee8a4dd9965873abba0a1112a94fc60b91
Dhairya Gupta <dhairyag23@gmail.com>
2025-03-09 12:23:51 -0500

    add PDU switching code for all pwm signals

--- Core/Src/pdu.c
+++ Core/Src/pdu.c
@@ -51,10 +51,18 @@
 #define SWITCH_BATTERY_FANS_GPIOX GPIOB
 #define SWITCH_BATTERY_FANS_PIN GPIO_PIN_15
 
+#define PWM_BATTERY_FANS_TIM htim8
+#define PWM_BATTERY_FANS CCR1
+#define TIM_BATTERY_FANS_CH TIM_CHANNEL_1
+
 #define FAULT_RAD_FANS_GPIOX GPIOD
 #define FAULT_RAD_FANS_PIN GPIO_PIN_10
 #define SWITCH_RAD_FANS_GPIOX GPIOC
 #define SWITCH_RAD_FANS_PIN GPIO_PIN_8
+
+#define PWM_RADIATOR_FANS_TIM htim8
+#define PWM_RADIATOR_FANS CCR3
+#define TIM_RADIATOR_FANS_CH TIM_CHANNEL_3
 
 #define FAULT_BOARD_POWER_GPIOX GPIOD
 #define FAULT_BOARD_POWER_PIN GPIO_PIN_9
@@ -66,25 +74,45 @@
 #define SWITCH_BRAKE_LIGHT_GPIOX GPIOD
 #define SWITCH_BRAKE_LIGHT_PIN GPIO_PIN_13
 
+#define PWM_BRAKE_LIGHT_TIM htim4
+#define PWM_BRAKE_LIGHT CCR2
+#define TIM_BRAKE_LIGHT_CH TIM_CHANNEL_2
+
 #define FAULT_COOLING_PUMP_1_GPIOX GPIOE // Battery Pumps
 #define FAULT_COOLING_PUMP_1_PIN GPIO_PIN_11
 #define SWITCH_COOLING_PUMP_1_GPIOX GPIOC
 #define SWITCH_COOLING_PUMP_1_PIN GPIO_PIN_7
+
+#define PWM_COOLING_PUMP_1 htim8
+#define PWM_COOLING_PUMP_CH CCR2
+#define TIM_COOLING_PUMP_1_INIT_CH TIM_CHANNEL_2
 
 #define FAULT_COOLING_PUMP_2_GPIOX GPIOD // Motor Pumps
 #define FAULT_COOLING_PUMP_2_PIN GPIO_PIN_8
 #define SWITCH_COOLING_PUMP_2_GPIOX GPIOC
 #define SWITCH_COOLING_PUMP_2_PIN GPIO_PIN_9
 
+#define PWM_COOLING_PUMP_2 htim8
+#define PWM_COOLING_PUMP_2_CH CCR4
+#define TIM_COOLING_PUMP_2_INIT_CH TIM_CHANNEL_4
+
 #define FAULT_GREEN_STATUS_GPIOX GPIOE
 #define FAULT_GREEN_STATUS_PIN GPIO_PIN_13
 #define SWITCH_GREEN_STATUS_GPIOX GPIOD
 #define SWITCH_GREEN_STATUS_PIN GPIO_PIN_15
 
+#define PWM_GREEN_STATUS_TIM htim4
+#define PWM_GREEN_STATUS CCR4
+#define TIM_GREEN_STATUS_CH TIM_CHANNEL_4
+
 #define FAULT_RED_STATUS_GPIOX GPIOE
 #define FAULT_RED_STATUS_PIN GPIO_PIN_10
 #define SWITCH_RED_STATUS_GPIOX GPIOD
 #define SWITCH_RED_STATUS_PIN GPIO_PIN_14
+
+#define PWM_RED_STATUS_TIM htim4
+#define PWM_RED_STATUS CCR3
+#define TIM_RED_STATUS_CH TIM_CHANNEL_3
 #endif
 
 void pdu_init(PDUData *pduData) {
@@ -115,7 +143,19 @@ void pdu_init(PDUData *pduData) {
     pduData->switches.green_status_light = 1.0f; // flash
     pduData->switches.red_status_light = 1.0f; // flash
 
-    HAL_TIM_PWM_Start(&htim8, TIM_CHANNEL_2); // just testing battery pump
+    /* Cooling */
+    HAL_TIM_PWM_Start(&PWM_COOLING_PUMP_1, TIM_COOLING_PUMP_1_INIT_CH);
+    HAL_TIM_PWM_Start(&PWM_COOLING_PUMP_2, TIM_COOLING_PUMP_2_INIT_CH);
+    HAL_TIM_PWM_Start(&PWM_BATTERY_FANS_TIM, TIM_BATTERY_FANS_CH);
+    HAL_TIM_PWM_Start(&PWM_RADIATOR_FANS_TIM, TIM_RADIATOR_FANS_CH);
+
+    /* Lights */
+    HAL_TIM_PWM_Start(&PWM_BRAKE_LIGHT_TIM, TIM_BRAKE_LIGHT_CH);
+    HAL_TIM_PWM_Start(&PWM_GREEN_STATUS_TIM, TIM_GREEN_STATUS_CH);
+    HAL_TIM_PWM_Start(&PWM_RED_STATUS_TIM, TIM_RED_STATUS_CH);
+
+
+
     init = true;
 }
 
@@ -129,7 +169,19 @@ void pdu_periodic(PDUData *pduData) {
     checkPDUFaults(pduData);
     checkPDUCurrents(pduData);
 
-    htim8.Instance->CCR2 = pduData->switches.cooling_pump_1 * htim8.Instance->ARR; // testing with percentages
+
+    /* Update PWM on all signals */
+    /* Cooling */
+    PWM_COOLING_PUMP_1.Instance->PWM_COOLING_PUMP_CH = (uint32_t) pduData->switches.cooling_pump_1 * PWM_COOLING_PUMP_1.Instance->ARR; // testing with percentages
+    PWM_COOLING_PUMP_2.Instance->PWM_COOLING_PUMP_2_CH = (uint32_t) pduData->switches.cooling_pump_2 * PWM_COOLING_PUMP_1.Instance->ARR; // testing with percentages
+
+    PWM_BATTERY_FANS_TIM.Instance->PWM_BATTERY_FANS = (uint32_t) pduData->switches.battery_fans * PWM_BATTERY_FANS_TIM.Instance->ARR;
+    PWM_RADIATOR_FANS_TIM.Instance->PWM_RADIATOR_FANS = (uint32_t) pduData->switches.battery_fans * PWM_RADIATOR_FANS_TIM.Instance->ARR;
+
+    /* Lights */
+    PWM_BRAKE_LIGHT_TIM.Instance->PWM_BRAKE_LIGHT = (uint32_t) pduData->switches.brake_light * PWM_BRAKE_LIGHT_TIM.Instance->ARR;
+    PWM_GREEN_STATUS_TIM.Instance->PWM_GREEN_STATUS = (uint32_t) pduData->switches.green_status_light * PWM_GREEN_STATUS_TIM.Instance->ARR;
+    PWM_RED_STATUS_TIM.Instance->PWM_RED_STATUS = (uint32_t) pduData->switches.red_status_light * PWM_RED_STATUS_TIM.Instance->ARR;
     // write out data to the CAN bus
 //    writePDUToCAN(pduData);
 }
